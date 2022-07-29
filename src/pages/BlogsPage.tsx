2dcd8f61242e38607523133bfae59c3e314205aa
jdavellanedag <jdavellaendag@gmail.com>
2022-07-30 01:49:46 +0200

    Header bug fixed

--- src/pages/BlogsPage.tsx
+++ src/pages/BlogsPage.tsx
@@ -1,6 +1,6 @@
 import { Outlet } from "react-router-dom";
 
-import NavBar, { NavBarLogo } from "../components/NavBar";
+import NavBar, { NavBarItem, NavBarLogo, NavBarMenu } from "../components/NavBar";
 import { FooterLinkBoxLayout } from "../layouts/Footer/FooterLinkBoxLayout";
 
 import MainLogo from "../assets/img/logo.png";
@@ -9,9 +9,16 @@ export const BlogsPage = () => {
   return (
     <>
       <header>
-        <NavBar className="nav__bar">
-          <NavBarLogo img={MainLogo} className="nav__bar-logo" goTo="home" />
-        </NavBar>
+        <div className="nav__container">
+          <NavBar className="nav__bar">
+            <NavBarLogo img={MainLogo} className="nav__bar-logo" goTo="home" />
+            <NavBarMenu className="nav__bar-menu">
+              <NavBarItem goTo="home" className="nav__bar-item txt-subtitle">
+                Home
+              </NavBarItem>
+            </NavBarMenu>
+          </NavBar>
+        </div>
       </header>
       <main>
         <Outlet />
