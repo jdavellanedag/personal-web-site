51d7808bba5ac599a4152e912e9048d67908428a
jdavellanedag <jdavellaendag@gmail.com>
2022-08-15 20:54:55 +0200

    Highlight code syntasix added

--- src/components/Blog/BlogEntry.tsx
+++ src/components/Blog/BlogEntry.tsx
@@ -1,9 +1,37 @@
 import ReactMarkdown from "react-markdown";
+import rehypeRaw from "rehype-raw";
+import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
+import { vscDarkPlus } from "react-syntax-highlighter/dist/cjs/styles/prism";
 
 interface Props {
   content: string;
 }
 
 export const BlogEntry = ({ content }: Props) => {
-  return <ReactMarkdown children={content} className="blog-entry" />;
+  return (
+    <ReactMarkdown
+      rehypePlugins={[rehypeRaw]}
+      components={{
+        code({ node, inline, className, children, ...props }) {
+          const match = /language-(\w+)/.exec(className || "");
+          return !inline && match ? (
+            <SyntaxHighlighter
+              children={String(children).replace(/\n$/, "")}
+              style={vscDarkPlus}
+              language={match[1]}
+              PreTag="div"
+              {...props}
+            />
+          ) : (
+            <code className={className} {...props}>
+              {children}
+            </code>
+          );
+        },
+      }}
+      className="blog-entry"
+    >
+      {content}
+    </ReactMarkdown>
+  );
 };
